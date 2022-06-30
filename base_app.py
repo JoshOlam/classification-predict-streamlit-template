b26737cd4c2c96b539d7c408206b4a02d4154931
macchime <75667985+macchime@users.noreply.github.com>
2022-06-30 11:32:33 +0100

    updated with visuals

--- base_app.py
+++ base_app.py
@@ -47,8 +47,6 @@ from scipy import sparse
 st.set_page_config(page_icon="resources/imgs/globe.png", page_title="Greener Cloud Global Warming Model")
 
 
-blank, title_col, blank = st.columns([2,3.5,2])
-title_col.title("Greener Cloud")
 
 primary_clr = st.get_option("theme.primaryColor")
 txt_clr = st.get_option("theme.textColor")
@@ -73,11 +71,16 @@ def main():
 	
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["General Information", "Classify Tweets", "Contact Us"]
+ 
+	options = ["General Information", 'Visuals', "Classify Tweets", "Contact Us"]
+	selection = st.sidebar.image(
+    "https://imgur.com/VIIqPzu.png",
+   	width=300,)
 	selection = st.sidebar.selectbox("Choose an Option Here:", options)
 
 	# Building out the "Information" page
 	if selection == "General Information":
+		st.image("https://imgur.com/zUvnHT1.png")
 		image = Image.open('resources/imgs/cloud_2.jpg')
 		st.image(image, caption = '...touching a Greener Cloud', use_column_width=True)
 		st.subheader("""Project Title: Climate Change Tweet Classification
@@ -117,10 +120,151 @@ def main():
 		if source_selection == 'Full_data':
 			st.write("Display of the full data(don't get overwhelmed :smiley:):")
 			st.write(raw)
+   
+   	# Building out the Visuals page
+	if selection == "Visuals":
+		st.image("https://imgur.com/HL0NhVQ.png")
+
+
+		visual_options = ["Visuals (Home)", "Bar Graphs", "Word Clouds", "Model performance"]
+		visual_options_selection = st.selectbox("Which visual category would you like?",
+		visual_options)
+
+		if visual_options_selection == "Visuals (Home)":
+			st.image('https://imgur.com/5QEtky5.png', width=730)
+   
+		if visual_options_selection == "Model performance":
+			per_listed = ['F1_measure', 'Fit_time']
+			per_list = st.selectbox('I would like to view the...', per_listed)
+
+			if per_list == 'F1_measure':
+				st.subheader('F1 scores of the various models used')
+				st.image('https://imgur.com/o1zYqC8.png', width=730)
+			if per_list == 'Fit_time':
+				st.subheader('Fit time of the various models used')
+				st.image('https://imgur.com/N4DVf2s.png', width=730)
+       
+			
+
+
+		if visual_options_selection == "Bar Graphs":
+			st.image('https://i.imgur.com/p3J5Gcw.png')
+
+			bar_nav_list = ['Sentiment distribution of raw data', 
+			'Most common words in various sentiment classes (raw data)', 
+			'Most common words in various sentiment classes (cleaned data)']
+
+			bar_nav = st.selectbox('I would like to view the...', bar_nav_list)
+
+
+			if bar_nav == 'Sentiment distribution of raw data':
+				st.subheader('Sentiment Distribution of Raw Data')
+				st.image('https://i.imgur.com/JT9HzVW.png', width=700)
+				st.write("This graph shows how the raw data was distruted amongst the various sentiment classes.")
+				st.write("The classes can be interpreted as follows:")				
+				st.write("2: News  --  the tweet links to factual news about climate change")
+				st.write("1: Pro  --  the tweet supports the belief of man-made climate change")
+				st.write("0: Neutral  --  the tweet neither supports nor refutes the belief of man-made climate change")
+				st.write("-1: Anti  --  the tweet does not believe in man-made climate change")
+		
+			if bar_nav == 'Most common words in various sentiment classes (raw data)':
+
+				raw_common_words_list = ['All tweets', 'Negative tweets', 'Positive tweets', 
+				'News-related tweets', 'Neutral tweets']
+				raw_common_words = st.radio('Raw Data Sentiment Classes:', raw_common_words_list)
+
+				if raw_common_words == 'All tweets':
+					st.subheader('Common Words in all Tweets')
+					st.image('https://i.imgur.com/HSdxDP9.png', width=700)
+
+				if raw_common_words == 'Negative tweets':
+					st.subheader('Common Words in Negative Tweets')
+					st.image('https://i.imgur.com/FqnrN1Y.png', width=700)
+
+				if raw_common_words == 'Positive tweets':
+					st.subheader('Common Words in Positive Tweets')
+					st.image('https://i.imgur.com/glF9Z0M.png', width=700)
+
+				if raw_common_words == 'News-related tweets':
+					st.subheader('Common Words in News-related Tweets')
+					st.image('https://i.imgur.com/fWDhrTL.png', width=700)
+
+				if raw_common_words == 'Neutral tweets':
+					st.subheader('Common Words in Neutral Tweets')
+					st.image('https://i.imgur.com/LEnkE9V.png', width=700)
+
+
+			if bar_nav == 'Most common words in various sentiment classes (cleaned data)':
+
+				clean_common_words_list = ['All tweets', 'Negative tweets', 'Positive tweets', 
+				'News-related tweets', 'Neutral tweets']
+				clean_common_words = st.radio('Cleaned Data Sentiment Classes:', clean_common_words_list)
+
+				if clean_common_words == 'All tweets':
+					st.subheader('Common Words in all Tweets')
+					st.image('https://i.imgur.com/1aOr9DD.png', width=700)
+
+				if clean_common_words == 'Negative tweets':
+					st.subheader('Common Words in Negative Tweets')
+					st.image('https://i.imgur.com/7Mp2NRX.png', width=700)
+
+				if clean_common_words == 'Positive tweets':
+					st.subheader('Common Words in Positive Tweets')
+					st.image('https://i.imgur.com/3SDTh6c.png', width=700)
+
+				if clean_common_words == 'News-related tweets':
+					st.subheader('Common Words in News-related Tweets')
+					st.image('https://i.imgur.com/sDFn7PF.png', width=700)
+
+				if clean_common_words == 'Neutral tweets':
+					st.subheader('Common Words in Neutral Tweets')
+					st.image('https://i.imgur.com/0ur3J3C.png', width=700)
+
+		if visual_options_selection == "Word Clouds":
+			st.image('https://i.imgur.com/QDhrJTR.png')
+
+			wc_nav_list = ['Most Common Words (Raw Data)', 
+			'Most Common Words (Cleaned Data)']
+
+			wc_nav = st.selectbox('I would like to view the...', wc_nav_list)
+
+
+			if wc_nav == 'Most Common Words (Raw Data)':
+				st.subheader('Most Common Words for all Tweets (raw data)')
+				st.image('https://i.imgur.com/MsGuWFv.png')
+
+			if wc_nav == 'Most Common Words (Cleaned Data)':
+
+				wc_clean_list = ['All tweets', 'Negative tweets', 'Positive tweets', 
+				'News-related tweets', 'Neutral tweets']
+				wc_clean = st.radio('Cleaned Data Sentiment Classes:', wc_clean_list)
+
+				if wc_clean == 'All tweets':
+					st.subheader('Common Words in all Tweets')
+					st.image('https://i.imgur.com/0MeELLk.png')
+
+				if wc_clean == 'Negative tweets':
+					st.subheader('Common Words in Negative Tweets')
+					st.image('https://i.imgur.com/fc0Aa9l.png')
+
+				if wc_clean == 'Positive tweets':
+					st.subheader('Common Words in Positive Tweets')
+					st.image('https://i.imgur.com/4LSqpBm.png')
+
+				if wc_clean == 'News-related tweets':
+					st.subheader('Common Words in News-related Tweets')
+					st.image('https://i.imgur.com/cmrDvhk.png')
+
+				if wc_clean == 'Neutral tweets':
+					st.subheader('Common Words in Neutral Tweets')
+					st.image('https://i.imgur.com/AJMYNOu.png')
+
 
 	#Build the "Classify Tweets" Page
 
 	if selection == "Classify Tweets":
+		st.image("https://imgur.com/HL0NhVQ.png")
+
 		st.info("Interact with our model by classifying some 'Single Tweets' or upload a '.csv' file with tweets to classify")
 		data_source = ['Select option', 'Single Tweet', 'Dataset'] #Defines the type of input to classify
 		source_selection = st.selectbox('Select your preferred data input option:', data_source)
